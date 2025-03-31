1e6f27a1823e1526902c691670e82e4410b53564
kimyoda <rladygks1210@naver.com>
2025-03-31 21:47:27 +0900

    [fix] css 수정 후 화면에 바로 나오도록 조치

--- mission/src/pages/movie/[[...id]].tsx
+++ mission/src/pages/movie/[[...id]].tsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
+import styles from "./movie-result.module.css";
 
 export default function Page() {
   const router = useRouter();
 
   const { id } = router.query;
 
-  return <h1>영화 상세페이지 {id} </h1>;
+  return <h1 className={styles.result}>영화 상세페이지 {id} </h1>;
 }
