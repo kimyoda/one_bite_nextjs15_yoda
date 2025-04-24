fcdc66c35178138e8c713d7689e5ee12de19a1cd
kimyoda <rladygks1210@naver.com>
2025-04-24 19:49:41 +0900

    [feat] 앱 라우터 데이터 페칭 missionday12

--- onebite_cinema_app_yoda/src/app/(with-searchbar)/page.tsx
+++ onebite_cinema_app_yoda/src/app/(with-searchbar)/page.tsx
@@ -1,25 +1,64 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 import movies from "@/dummy.json";
+import { MovieData } from "@/type";
+
+// 모든 영화를 가져오는 컴포넌트
+async function AllMovies() {
+  // API 서버에서 모든 영화 데이터를 가져온다.
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다..!</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+
+  return (
+    // 해당 div에 직접 grid 스타일 클래스 적용
+    <div className={style.all_container}>
+      {allMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+// 추천 영화를 가져오는 컴포넌트
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다!</div>;
+  }
+
+  const recoMovies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.reco_container}>
+      {recoMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={style.conatiner}>
+    <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.reco_conatiner}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`reco-${movie.id}`} {...movie} />
-          ))}
-        </div>
+
+        <RecoMovies />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={style.all_container}>
-          {movies.map((movie) => (
-            <MovieItem key={`all-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        {/* ✅ 여기엔 따로 div 감싸지 않고 컴포넌트 그대로 호출 */}
+        {/* AllMovies 내부에서 직접 grid를 적용하고 있으므로 불필요 */}
+        <AllMovies />
       </section>
     </div>
   );
--- onebite_cinema_app_yoda/src/app/(with-searchbar)/search/page.tsx
+++ onebite_cinema_app_yoda/src/app/(with-searchbar)/search/page.tsx
@@ -1,8 +1,26 @@
 import MovieItem from "@/components/movie-item";
-import movies from "@/dummy.json";
 import style from "./page.module.css";
+import { MovieData } from "@/type";
 
-export default function Page() {
+export default async function Page({
+  searchParams, // URL의 쿼리 파라미터를 받는 객체
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  // 검색어 초출
+  const { q } = await searchParams;
+
+  // API 서버에 검색 요청을 보내고 결과를 가져온다
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다!!</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+
   return (
     <div className={style.container}>
       {movies.map((movie) => (
